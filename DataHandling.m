0bbfe923c22b471c596c3f95529de16e4232e38f
Michael Jensen <michaeltomjensen@gmail.com>
2015-04-28 13:34:02 +1000

    Highlights anomalies

---
+++ DataHandling.m
@@ -0,0 +1,13 @@
+function [ output_args ] = DataHandling( input_args )
+%DATAHANDLING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
+function [columnMap] = mapColumn(column)
+%MAPCOLUMN returns a container.Map of a column
+% map[unique,frequency]
+
+end
+
